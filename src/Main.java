bd76aa3a6f1462bb5aea77495275110836e49404
Evgeniy Viter <viter.evgeniy@gmail.com>
2024-02-24 19:20:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         int amountStarted = 100;
-        int refill = 1100;
+        int refill = 800;
         int amountTotal1 = (amountStarted + refill);
-        if (refill > 1000) {
-            int bonus = (refill / 100);
+        int percent;
+        if (amountTotal1 >= 1000) {
+            percent = 1;
+        } else {
+                percent = 0;
+            }
+            int bonus = amountTotal1 * percent / 100;
             int amountTotal2 = (amountTotal1 + bonus);
+
             System.out.println("Счет: " + amountTotal1);
             System.out.println("Бонусы: " + bonus);
             System.out.println("Итоговый счет: " + amountTotal2);
 
-        }
+
     }
 }
